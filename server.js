554d5806151081a48c1e2a1cee3e2e62f4dda136
rahul-cs7 <rahuluu2327@gmail.com>
2026-04-25 21:41:35 +0530

    Fixed port for deployment

--- server.js
+++ server.js
@@ -89,7 +89,8 @@ app.use((req, res) => {
 });
 
 // ================= SERVER =================
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
